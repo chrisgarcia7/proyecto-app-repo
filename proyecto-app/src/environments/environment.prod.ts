4591bcae16b872e196ba079d547abd9f75a68556
chrisgarcia7 <garciachristian9@yahoo.com>
2025-03-01 18:30:33 -0600

    Firebase Instalado

--- proyecto-app/src/environments/environment.prod.ts
+++ proyecto-app/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseconfig: {
+    projectId: 'proyectodm2-e4e95',
+    appId: '1:541142127125:web:e6c7bcdfa80f75acc1741c',
+    storageBucket: 'proyectodm2-e4e95.firebasestorage.app',
+    apiKey: 'AIzaSyAjW2xWJa4DFl_I_HBr3nxCq1od1TI2kB0',
+    authDomain: 'proyectodm2-e4e95.firebaseapp.com',
+    messagingSenderId: '541142127125',
+  }
 };
+
--- proyecto-app/src/main.ts
+++ proyecto-app/src/main.ts
@@ -1,14 +1,33 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { environment } from './environments/environment.prod';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideFirebaseApp(() =>
+      initializeApp(environment.firebaseconfig)
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideMessaging(() => getMessaging()),
   ],
 });
