087f22e5f07b9edc487a8d10b80fc8feba750508
chrisgarcia7 <garciachristian9@yahoo.com>
2025-03-16 15:13:41 -0600

    Agregando boton flotante

--- proyecto-app/src/app/gallery/ui/pages/gallery/gallery.page.ts
+++ proyecto-app/src/app/gallery/ui/pages/gallery/gallery.page.ts
@@ -1,21 +1,23 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonFab, IonFabButton, IonFabList, IonIcon } from '@ionic/angular/standalone';
 import { GalleryService } from 'src/app/gallery/services/gallery.service';
 import { ProfileDto } from 'src/app/auth/modelos/profile';
 import { AuthService } from 'src/app/auth/services/auth.service';
+import { IconService } from 'src/app/shared/services/icons/icon.service';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.page.html',
   styleUrls: ['./gallery.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonIcon, IonFabList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonFab, IonFabButton]
 })
 export class GalleryPage implements OnInit {
   private readonly _authService: AuthService = inject(AuthService)
   private readonly _galleryService: GalleryService = inject(GalleryService)
+  private readonly _iconservice: IconService= inject(IconService);
   
   
   userImages: string[] = [];
@@ -38,6 +40,21 @@ export class GalleryPage implements OnInit {
     } else {
         console.log('No user is logged in.');
     }
+  }
+
+  onFabClick() {
+    // Lógica para el clic del botón principal
+    console.log('Floating action button clicked!');
+  }
+
+  actionOne() {
+    // Lógica para la acción uno
+    console.log('Action one clicked!');
+  }
+
+  actionTwo() {
+    // Lógica para la acción dos
+    console.log('Action two clicked!');
   }
 
 
--- proyecto-app/src/app/shared/services/icons/icon.service.ts
+++ proyecto-app/src/app/shared/services/icons/icon.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { addIcons } from 'ionicons';
-import { arrowBackOutline, folderSharp, homeSharp, personSharp } from 'ionicons/icons';
+import { addOutline, arrowBackOutline, folderSharp, homeSharp, personSharp } from 'ionicons/icons';
 import { personOutline } from 'ionicons/icons';
 import { atOutline } from 'ionicons/icons';
 import { lockClosedOutline } from 'ionicons/icons';
@@ -23,6 +23,7 @@ export class IconService {
         addIcons({ callOutline });
         addIcons({homeSharp});
         addIcons({folderSharp});
-        addIcons({personSharp})
+        addIcons({personSharp});
+        addIcons({addOutline});
   }
 }
